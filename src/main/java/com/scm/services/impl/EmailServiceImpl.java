eae25ee1ff0d0b7bff2139054d8105a548aeafb8
shreyash2206 <shreyash112203@gmail.com>
2024-12-18 00:28:02 +0530

    Added attachment feature

--- src/main/java/com/scm/services/impl/EmailServiceImpl.java
+++ src/main/java/com/scm/services/impl/EmailServiceImpl.java
@@ -4,7 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.scm.services.EmailService;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 
 import com.scm.services.EmailService;
 
